a1562dcb740f9c581346f1cf92054146375eade9
EricZhang12138 <2067716083zyo@gmail.com>
2026-02-01 22:56:00 +0000

    finished the order matching logic for when a bid is made

--- orderbook.cpp
+++ orderbook.cpp
@@ -92,7 +92,92 @@ orderResult Orderbook::match(int64_t price, int64_t volume, bool side, int64_t t
             }
             global_map[orderID] = new_order;
             return res;
+        }else{
+            res.fulfilled = true;
+            return res;
         }
+    
+    }else{ // we add to bids map, meaning we want to buy
+        orderResult res = {
+            .orderID = orderID,
+            .fulfilled = false,
+            .remainingQty = volume
+        };
+        while(!asks.empty() && asks.begin() -> first <= price && volume > 0){
+            std::unique_ptr<Limit>& lowest_limit = asks.begin() -> second; // Look at the pointer in the map but don't copy it
+            while(lowest_limit -> count > 0 && volume > 0){
+                Order& current_order = *(lowest_limit -> head);
+                if (current_order.volume > volume){
+                    current_order.volume -= volume; // decrement the volume of the order
+                    lowest_limit -> totalVolume -= volume;
+                    Fill f_1 = {
+                        .price = current_order.price,
+                        .volume = volume,
+                        .timestamp = time
+                    };
+                    res.fulfilled = true;
+                    res.remainingQty = 0;
+                    res.trades.push_back(f_1);
+
+                    return res; // this is when we return the result 
+
+                }else{ // if(current_order.volume <= volume)
+                    // update Limit
+                    lowest_limit -> count -= 1; // number of order decrements by 1
+                    lowest_limit -> totalVolume -= current_order.volume;
+                    lowest_limit -> head = lowest_limit -> head -> next; // we move the pointer to the next Order
+                    volume -= current_order.volume; // update the current volume count
+                    res.remainingQty -= current_order.volume;
+                    Fill f_1 = {
+                        .price = current_order.price,
+                        .volume = current_order.volume,
+                        .timestamp = time
+                    };
+                    if (lowest_limit -> head != nullptr){
+                        lowest_limit -> head -> prev = nullptr; 
+                    }
+                    global_map.erase(current_order.orderID);
+                    delete &current_order;
+                    res.trades.push_back(f_1);
+                    //after all the processing if count == 0, then we need to erase this price level in the map
+                    if (lowest_limit -> count == 0){
+                        // Method 2: Convert reverse iterator
+                        asks.erase(asks.begin()); // erase the entire entry 
+                        break; // leave the while loop, and we don't want to check highest_limit -> count > 0 ever again bc it doesn't exist
+                    }
+                }
+            }
+        }
+        if (volume > 0){ // if the volume is 0, we don't want to create a zombie order
+            // now we have gone through all the Limit in the bids that satisfy the condition
+            // we need to insert the remaining asks into the tree
+            Order* new_order = new Order{price,volume,orderID, side}; 
+            // using new Order() will require you to have your own constructor
+            // but using new Order{} allows you to directly initialise those variables
+            auto it = bids.find(price);
+            if (it != bids.end()){ // price is in the asks map
+                std::unique_ptr<Limit>& insert_limit = it -> second;
+                insert_limit -> count ++;
+                insert_limit -> tail -> next = new_order;
+                new_order -> prev = insert_limit -> tail;
+                insert_limit -> tail = new_order;
+                insert_limit -> totalVolume += volume;
+                new_order -> parentlimit = (it -> second).get();
+            }else{// price is not in the asks map
+                bids[price] = std::make_unique<Limit>();
+                bids[price] -> count = 1;
+                bids[price] -> head = new_order;
+                bids[price] -> tail = new_order;
+                bids[price] -> totalVolume = volume;
+                new_order -> parentlimit = bids[price].get();
+            }
+            global_map[orderID] = new_order;
+            return res;
+        }else{
+            res.fulfilled = true;
+            return res;
+        }
+
     }
 }
 
